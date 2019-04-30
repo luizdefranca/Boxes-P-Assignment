9c745261e98cf0f4db5ff73f011a100d3d350602
LuizRamos <luizramospe@hotmail.com>
2019-04-30 12:28:31 -0400

    Finished method  howManyTimesFitInsideWithBox. Done

--- Boxes (P*) Assignment/main.m
+++ Boxes (P*) Assignment/main.m
@@ -13,7 +13,10 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
         Box *box1 = [[Box alloc]initWithHeight:1 Width:1 Lenght:1];
-        NSLog(@"%f", [box1 volume]);
+        NSLog(@"The box1 has %.2f volume", [box1 volume]);
+        
+        Box *box2 = [[Box alloc] initWithHeight:2 Width:2 Lenght:2];
+        NSLog(@"The box1 fits %.2f times inside box2", [box2 howManyTimesFitInsideWithBox: box1]);
         
         
     }
